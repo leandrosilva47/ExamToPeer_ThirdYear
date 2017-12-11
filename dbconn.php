7a5687939d10c3cefead79ff4e45521c18165539
Basha1 <smahamed8@hotmail.com>
2017-12-11 00:42:11 +0000

    Database

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,7 @@
 <?php
-$dbServername = "localhost";
-$dbUsername = "mobasha";
-$dbPassword = "exam";
-$dbName = "examtopeer";
+$dbServername = "192.168.0.4";
+$dbUsername = "ciara";
+$dbPassword = "1LoQrvab";
+$dbName = "gopagoda";
 
 $con = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
